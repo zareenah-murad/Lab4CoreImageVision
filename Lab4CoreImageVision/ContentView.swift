f4369a327b62eb00a9600e5876c5a012aa1db966
zareenah <zmurad@smu.edu>
2024-10-29 11:08:00 -0500

    Initial Commit

---
+++ Lab4CoreImageVision/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab4CoreImageVision
+//
+//  Created by Zareenah Murad on 10/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab4CoreImageVision/Lab4CoreImageVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab4CoreImageVisionApp.swift
+//  Lab4CoreImageVision
+//
+//  Created by Zareenah Murad on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab4CoreImageVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
